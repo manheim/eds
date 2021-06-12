8b7cc59326132437b65eb4c7df45bef983f08a5a
qwcode <qwcode@gmail.com>
2021-06-12 01:01:00 -0700

    more eds.plugin tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -24,3 +24,13 @@ def test_get_child_plugins():
     assert len(p.descendants) == 2
     assert type(p.descendants[0]).__name__ == 'PluginChild'
     assert type(p.descendants[1]).__name__ == 'PluginParent'
+
+
+def test_id_property():
+    p = Plugin({'id': 'my_id'})
+    assert p.id == 'my_id'
+
+
+def test_yaml_property():
+    p = Plugin({'some': 'yaml'})
+    assert p.yaml == {'some': 'yaml'}
