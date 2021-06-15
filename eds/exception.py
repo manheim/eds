3c9164e88d4bb68f3c971e43553bcb8c51915aa9
qwcode <qwcode@gmail.com>
2021-06-14 20:46:38 -0700

    docstrings for eds.exception

--- eds/exception.py
+++ eds/exception.py
@@ -1,26 +1,29 @@
+"""eds.exception module."""
+
+
 class EdsError(Exception):
-    """EDS base exception"""
+    """EDS base exception."""
 
 
 class DuplicateIncludeError(Exception):
-    """Raised when a duplicate include occurs"""
+    """Raised when a duplicate include occurs."""
 
 
 class PluginNameNotFoundError(EdsError):
-    """Raised when a specific plugin is not found"""
+    """Raised when a specific plugin is not found."""
 
 
 class PluginNameMismatchError(EdsError):
-    """Raised when a plugin name does not match the 'name' attribute of the object"""
+    """Raised when a plugin name does not match the 'name' attribute of the object."""
 
 
 class DuplicatePluginError(EdsError):
-    """Raised when a specific name has multiple plugins"""
+    """Raised when a specific name has multiple plugins."""
 
 
 class NoPluginsFoundError(EdsError):
-    """Raised when no template plugins are found"""
+    """Raised when no template plugins are found."""
 
 
 class PluginNoNameAttributeError(EdsError):
-    """Raised when a plugin has no 'plugin_name' attribute"""
+    """Raised when a plugin has no 'plugin_name' attribute."""
