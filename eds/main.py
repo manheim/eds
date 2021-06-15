c8fd0afcf1f102801b4fa74e5b3cfd910be71163
qwcode <qwcode@gmail.com>
2021-06-14 20:44:43 -0700

    annotations and docstrings for eds.cli

--- eds/main.py
+++ eds/main.py
@@ -1,17 +1,30 @@
-from eds.exception import EdsError
+"""eds.main module."""
+
+from __future__ import annotations
 from eds.interfaces.worker import Worker
+from eds.event import Event
 from eds.extend import get_plugin
 from eds.project import Project
 
 
-def main(event):
+def main(event: Event) -> None:
+    """The main routine to process events.  Includes exception logging.
+
+    Args:
+        event (Event): The commit event to process.
+    """
     try:
         process(event)
-    except EdsError:
+    except Exception:
         pass
 
 
-def process(event):
+def process(event: Event) -> None:
+    """The main routine to process events.
+
+    Args:
+        event (Event): The commit event to process.
+    """
     worker = get_plugin(Worker.interface_name, event.worker_plugin)
     if not event.eds_built:
         worker.build_eds(event.eds_version)
