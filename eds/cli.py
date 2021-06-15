2a10275e407e775622c5e5f6e04f6e4e08911e12
qwcode <qwcode@gmail.com>
2021-06-14 20:39:02 -0700

    annotations and docstrings for eds.cli

--- eds/cli.py
+++ eds/cli.py
@@ -1,6 +1,9 @@
+"""eds.cli module."""
+
 from eds.event import Event
 from eds.main import main
 
 
-def cli():
+def cli() -> None:
+    """CLI entry point to main."""
     main(Event.init_from_local())
