15d9fdf75dcb97255d82a3e9c73d517371b0f819
James Leopold <james.leopold@coxautoinc.com>
2021-06-21 09:49:50 -0400

    init

--- eds/interfaces/vcs_provider.py
+++ eds/interfaces/vcs_provider.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
 from eds.interfaces.plugin import Plugin
-
+from abc import ABCMeta, abstractmethod
 
 class VcsProvider(Plugin):
     """eds.vcs_provider interface."""
@@ -16,22 +16,45 @@ class VcsProvider(Plugin):
         "properties": {}
     }
 
+    @abstractmethod
     def parse_event(self) -> Dict:
         """Parse webhook event for project url and ref."""
         pass
 
+    @abstractmethod
     def get_files(self) -> Dict:
         """Get project files."""
         pass
 
+    @abstractmethod
     def create_project(self) -> None:
         """Create a Project."""
         pass
 
+    @abstractmethod
     def delete_project(self) -> None:
         """Delete a Project."""
         pass
 
+    @abstractmethod
     def update_project(self) -> None:
         """Update a Project."""
         pass
+
+class GithubEnterpriseProvider(VcsProvider):
+    def parse_event(self) -> Dict:
+        return super().parse_event()
+
+    def get_files(self) -> Dict:
+        return super().get_files()
+
+    def create_project(self) -> None:
+        return super().create_project()
+
+    def delete_project(self) -> None:
+        return super().delete_project()
+
+    def update_project(self) -> None:
+        return super().update_project()
+
+VcsProvider.register(GithubEnterpriseProvider)
