5716668ec6c2fdded08160b5566bd682f4e00295
James Leopold <james.leopold@coxautoinc.com>
2021-06-22 11:36:47 -0400

    typo fix

--- eds/interfaces/worker.py
+++ eds/interfaces/worker.py
@@ -8,7 +8,7 @@ class Worker(Plugin):
 
     schema = {
         "$schema": "https://json-schema.org/draft/2020-12/schema",
-        "$id": "eds.worked",
+        "$id": "eds.worker",
         "title": "Worker",
         "type": "object",
         "properties": {}
