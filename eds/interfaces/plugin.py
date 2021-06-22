d819f6145e30a23e3a5412a4ccb20ae5e20ed7dd
James Leopold <james.leopold@coxautoinc.com>
2021-06-22 09:46:46 -0400

    update plugin interface

--- eds/interfaces/plugin.py
+++ eds/interfaces/plugin.py
@@ -1,23 +1,25 @@
 
 from __future__ import annotations
 from typing import List, Dict
+from abc import ABCMeta, abstractmethod
 
-
-class Plugin():
+class Plugin(metaclass=ABCMeta):
     """general plugin interface."""
 
     schema: Dict = {}
     """json schema for plugin properties"""
 
     @property
+    @abstractmethod
     def children(self) -> List[Plugin]:
         """The list of child plugins.
 
         Returns:
             List[Plugin]: The list of child plugins.
         """
-        return []
+        pass
 
+    @abstractmethod
     def validate(self) -> None:
         """Validate the plugin."""
         pass
